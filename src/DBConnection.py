a3aebf36899a22fb91b1c8256dbe40a2e95cae38
antowkas <antonsokolov.ac@gmail.com>
2023-11-26 12:33:35 +0300

    Добавление функции insert

--- src/DBConnection.py
+++ src/DBConnection.py
@@ -1,6 +1,7 @@
 import sqlite3
 from os.path import isfile
 from sqlite3.dbapi2 import Cursor
+from re import match as re_match
 
 
 class DBConnect:
@@ -45,8 +46,31 @@ class DBConnect:
         return self.execute(open(path, encoding="UTF-8").read())
 
     @cursor_decorator
-    def execute(self, response: str, cur: Cursor) -> list:
-        return cur.execute(response).fetchall()
+    def execute(self, request: str, cur: Cursor) -> list:
+        return cur.execute(request).fetchall()
+
+    @staticmethod
+    def test_string(string) -> bool:
+        """
+        True - хорошая строка
+        False - плохая строка
+        """
+        pattern = r'^[^;\'"\\]*$'
+        return bool(re_match(pattern, string))
+
+    def insert(self, table: str, values_list: list[tuple], columns_name: tuple = None) -> None:
+        if not self.test_string(table):
+            raise ValueError("Bad table name:", table)
+        request = f"INSERT INTO {table} "
+        if columns_name is not None:
+            request += f"({', '.join(columns_name)}) "
+        request += "VALUES "
+        for values in values_list:
+            for value in values:
+                if isinstance(value, str) and not self.test_string(value):
+                    raise ValueError("Bad value:", value)
+        request += ', '.join(map(repr, values_list))
+        self.execute(request)
 
 
 if __name__ == '__main__':
