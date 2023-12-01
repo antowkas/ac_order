49f4b54e7975c1a4c5bb6a54319bd97400263af8
KirillSyber <kirill-145k@mail.ru>
2023-12-01 13:45:54 +0300

    Поиск по критериям

--- src/sql/TermCriteriaF.sql
+++ src/sql/TermCriteriaF.sql
@@ -5,7 +5,7 @@ FROM 'Journal'
 JOIN 'Order' ON 'Journal'.'order_id' = 'Order'.'order_id'
 JOIN 'Product' ON 'Journal'.'product_id' = 'Product'.'product_id'
 JOIN 'Category'ON 'Product'.'category_id' = 'Category'.'category_id'
-JOIN 'Fabricator' ON 'Product'.'fabricator_id' = 'Fabricator'.'fabricator_id';
+JOIN 'Fabricator' ON 'Product'.'fabricator_id' = 'Fabricator'.'fabricator_id'
 WHERE 'Order'.'order_id' LIKE '%{}%' AND 'Product'.'product_name' LIKE '%{}%'
 AND 'journal'.'quantity' LIKE '%{}%' AND 'Fabricator'.'fabricator_name' LIKE '%{}%'
 AND 'Category'.'category_name' LIKE '%{}%'
